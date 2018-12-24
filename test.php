e85dcab8a5edea7f0e67a4db8598686a4820ffc1
Igor <igor@igor.areal>
2018-12-24 10:53:21 +0300

    test

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Библиотека");
-?><?$APPLICATION->IncludeComponent(
+?>
+
+<?$APPLICATION->IncludeComponent(
 	"bitrix:breadcrumb",
 	"",
 Array()
